e3462d38d3d96c4cc4d0bea9a10ed75b845d19a9
Felicia Weathers <Fel.Weathers@gmail.com>
2016-10-30 17:02:19 -0400

    Get data from url and print jsonResults from processed data

--- ViewController.swift
+++ ViewController.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=481d88783d376954dbbebf3d1bedbe8a")!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if error != nil {
+                
+                print(error)
+                
+            } else {
+                
+                if let urlContent = data {
+                    
+                    do {
+                        
+                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        
+                        print(jsonResult)
+                        
+                    } catch {
+                        
+                        print("JSON Processing Failed")
+                        
+                    }
+                }
+            }
+        }
+        task.resume()
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
